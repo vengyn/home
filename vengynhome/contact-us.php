31b1c2f34545ad991fa23bc0aa55f3390e6fdb35
alexandertreubig <alexandertreubig@tests-mbp-8>
2015-06-29 18:06:55 -0400

    closed div

--- vengynhome/contact-us.php
+++ vengynhome/contact-us.php
@@ -105,11 +105,10 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 									<a href="#" class="image fit"><img src="http://aceshightattooshop.com/wp-content/uploads/2014/06/profile-placeholder.jpg" alt="" /></a>							
 									<p>Aliquam massa urna, phppppppp imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
 									</section>
+									</div>
 								</div>
 							</div>
 						</div>
-						</div>
-					</div>
 
 										<!-- Footer -->
 										<footer id="footer">
