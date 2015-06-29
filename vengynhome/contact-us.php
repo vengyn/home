6e6a378c9f66843b6eb509f04fa05f0bb7f94d18
h0lybyte <kbve.ceo@gmail.com>
2015-06-29 18:18:12 -0400

    Update Fix

--- vengynhome/contact-us.php
+++ vengynhome/contact-us.php
@@ -101,13 +101,13 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 									<div style="width:100%;">
 										
 										
-				<div style="float:left; width:20%;">
-				<a href="#" class="image fit"><img src="http://aceshightattooshop.com/wp-content/uploads/2014/06/profile-placeholder.jpg" alt="" /></a>							
-				</div>	
-				<div style="float:right; width:80%;">
-										<p>Aliquam massa urna, phppppppp imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
-				</div>					
-									
+										<div style="float:left; width:50%;">
+										<a href="#" class="image fit"><img src="http://aceshightattooshop.com/wp-content/uploads/2014/06/profile-placeholder.jpg" alt="" /></a>							
+										</div>	
+										<div style="float:right; width:50%;">
+																<p>Aliquam massa urna, phppppppp imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
+										</div>					
+															
 									</div>
 								
 							</div>
