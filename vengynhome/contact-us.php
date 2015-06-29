1150b1f20b77aca5de949fb6f963117768fbe9a8
alexandertreubig <alexandertreubig@tests-mbp-8>
2015-06-29 18:24:02 -0400

    border 1px

--- vengynhome/contact-us.php
+++ vengynhome/contact-us.php
@@ -105,6 +105,7 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 										<a href="#" class="image fit"><img src="http://aceshightattooshop.com/wp-content/uploads/2014/06/profile-placeholder.jpg" alt="" /></a>							
 										</div>	
 										<div style="float:center; width:20%;">
+										border-right: 1px solid white;
 										</div>
 										<div style="float:right; width:40%;">
 										<p>Aliquam massa urna, phppppppp imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
