7a3ec9dace2c15e297948d074eb5ed954a542ddf
alexandertreubig <alexandertreubig@tests-mbp-8>
2015-06-29 17:24:29 -0400

    all the ppppppps

--- vengynhome/contact-us.php
+++ vengynhome/contact-us.php
@@ -102,7 +102,7 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 									<a href="#" class="image fit"><img src="http://aceshightattooshop.com/wp-content/uploads/2014/06/profile-placeholder.jpg" alt="" /></a>							
 								</div>
 								<div class="6u$ 12u$(medium) important(medium)">
-									<p>Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
+									<p>Aliquam massa urna, phppppppp imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
 								</div>
 							</div>
 						</div>
